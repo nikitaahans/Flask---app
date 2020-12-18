780a217a6a957f43aba45f40dcd49fdc80025804
nikitaahans <nikitahans6@gmail.com>
2020-12-18 20:44:27 +1100

    create flask app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+import flask
+print(flask.__version__)
 
+from flask import Flask
+app = Flask(__name__)
 
-
-
-
-
-
-def hello():
-    print('Hello')
+@app.route('/')
+def home():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    hello()
+    app.run()
+
