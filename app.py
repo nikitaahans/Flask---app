7ede3858b05837848d6ad17a6270f35585dc6c2a
nikitaahans <nikitahans6@gmail.com>
2020-12-19 16:36:53 +1100

    about.html file

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ import flask
 print(flask.__version__)
 
 from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#     return 'Hello World!'
+#
+# if __name__ == '__main__':
+#     app.run()
+
+# Adding HTML to the page
+from flask import render_template
+
 app = Flask(__name__)
-
+#
 @app.route('/')
+def about():
+    return 'Hello World'
+
+@app.route('/home/')
 def home():
-    return 'Hello World!'
+    return render_template('about.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-
