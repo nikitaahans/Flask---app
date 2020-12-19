1879e131d84cb26db7249da19c7abdfc47915254
nikitaahans <nikitahans6@gmail.com>
2020-12-19 16:37:25 +1100

    added html page

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ import flask
 print(flask.__version__)
 
 from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return 'Hello World!'
-#
-# if __name__ == '__main__':
-#     app.run()
 
 # Adding HTML to the page
 from flask import render_template
